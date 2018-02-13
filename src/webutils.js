9b6fa1956eefa86ec5309d6e8294a9efa92d35a9
Imtiaz Haque <bhubon047@gmail.com>
2018-02-13 17:31:10 -0500

    wrote helper functions

--- src/webutils.js
+++ src/webutils.js
@@ -1,1 +1,74 @@
 /* webutils.js */
+const path=require('path');
+const fs=require('fs');
+
+function getExtension(filename){
+
+	let extension=path.extname(filename);
+	extension=extension.slice(1,extension.length);
+
+	return extension.toLowerCase();
+
+}
+
+function textResponse(err,data,sock,extension){
+
+	if(err){
+
+		sock.write(`HTTP/1.1 404 OK\r\nContent-Type:text/plain\r\n\r\nproblem reading file`);
+
+	}
+	else{
+
+		sock.write(`HTTP/1.1 200 OK\r\nContent-Type:text/${extension}\r\n\r\n${data}`)
+	}
+
+	sock.close();
+
+}
+
+function sendTextFile(filename,sock){
+
+	let extension=getExtension(filename);
+	const absolutePath=path.join(__dirname,'..','/public',filename);
+
+	fs.readFile(absolutePath,'utf8',(err,data)=>{textResponse(err,data,sock,extension)})
+
+}
+
+function imgResponse(err,data,sock,extension){
+
+	if(err){
+
+		sock.write(`HTTP/1.1 404 OK\r\nContent-Type:text/${extension}\r\n\r\nProblem reading file`);
+	}
+	else{
+
+		sock.write(`HTTP/1.1 200 OK\r\nContent-Type:text/${extension}\r\n\r\n`);
+		sock.write(data);
+	}
+
+	sock.close();
+
+
+}
+
+function sendImage(filename, sock){
+
+	let extension=getExtension(filename);
+	const absolutePath=path.join(__dirname,'..','public',filename);
+
+	fs.readFile(absolutePath,(err,data)=>{imgResponse(err,data,sock,extension)});
+
+}
+
+console.log(path.join(__dirname,'..','/public','/css/base.css'))
+
+module.exports={
+
+	getExtension: getExtension,
+	sendTextFile: sendTextFile,
+	sendImage:sendImage
+
+
+}
