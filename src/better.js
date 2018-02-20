f483b50474ed96bc84945d0e6b32dcf581ca2bb1
Imtiaz Haque <bhubon047@gmail.com>
2018-02-20 12:32:44 -0500

    started better.js

--- src/better.js
+++ src/better.js
@@ -1,1 +1,55 @@
 /* better.js */
+
+const App=require('./webframework.js').App
+const app=new APP();
+
+
+app.get('/', function(req,res){
+
+
+	res.sendFile('site.html');
+
+})
+
+app.get('/form', function(req,res){
+
+
+	res.sendFile('form.html');
+
+})
+
+app.get('/rando', function(req,res){
+
+
+	res.redirect(301,'/');
+
+})
+
+
+app.get('/css/base.css',function(req,res){
+	
+	res.sendFile('base.css');
+})
+
+app.get('/image1.jpg',function(req,res){
+
+	res.sendFile('image1.jpg');
+
+})
+
+app.get('/image2.gif'function(req,res){
+
+	res.sendFile('image.gif');
+})
+
+app.get('/image3.jpg'function(req,res){
+
+	res.sendFile('image3.jpg');
+})
+
+app.post('/form',function(req,res){
+
+	res.write('HTTP/1.1 200 OK\r\n\r\n' + req.body);
+})
+
+app.listen(8080, '127.0.0.1')
